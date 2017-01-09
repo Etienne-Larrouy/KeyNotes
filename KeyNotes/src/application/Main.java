39749dcabaeb79296bf3d2a3a253d98caf76e37e
e152234 <e152234@E-0620.univ-lemans.fr>
2017-01-09 09:30:14 +0100

    Initialisation du projet

---
+++ KeyNotes/src/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try { 
+			AnchorPane page = (AnchorPane) FXMLLoader.load(Main.class.getResource("../Keynotes.fxml"));
+			Scene scene = new Scene(page);
+
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("KeyNotes");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
